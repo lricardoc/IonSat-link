c2b67ad486b07b9899ae37cfa5de795b5c8fd5e3
nouranne-bd <bouzad.nourane@gmail.com>
2021-07-15 10:53:12 +0200

    version 3

--- linkbudgetdownlink.m
+++ linkbudgetdownlink.m
@@ -1,4 +1,4 @@
-function [linkmargin] = linkbudgetuplink(sc_antenna_gain)
+function [linkmargin] = linkbudgetdownlink(sc_antenna_gain)
 
 %   Detailed explanation goes here
 
@@ -9,14 +9,12 @@ a = 6678;
 alt_orbit=a*1000-R_earth;
 d_0=(sqrt((R_earth+alt_orbit)^2-R_earth^2))/1000;
 slant_range=sqrt(R_earth^2+(R_earth+alt_orbit)^2 - (2*R_earth*(R_earth+alt_orbit)*cos(acos(R_earth/(R_earth+alt_orbit))*cos(Coelevation)-Coelevation)));
-frequency=2100e6; %Hz
+frequency=2200e6; %Hz
 c=3e8; %m/s celerity
 boltzmann=-226.8;
 
-sc_antenna_gain=gainHF(,3);
-% sc_figureofmerrit=u(2);
-% sc_SNR=u(3);
-% Telecommand_System_ebno=u(4);
+sc_antenna_gain=gainHF(:,3);
+
 
 %Ground Station
 gs_transmitter_power=11.8; % in Watts
@@ -29,29 +27,31 @@ gs_EIRP=gs_dBW+gs_antenna_gain-gs_total_tranmission_line_losses;
 sc_transmitter_power=2; % in Watts
 sc_dBW=10*log10(sc_transmitter_power);
 sc_total_tranmission_line_losses=2; %in dB
+loss_from_duplexer_insertion=1; %in dB
+double_antenna_transmission_losses=3; %in dB
 %sc_antenna_gain=-1; %in dBi at 5° elevation
-%sc_EIRP=sc_antenna_gain+sc_dBW-sc_total_tranmission_line_losses;
+sc_EIRP=sc_antenna_gain+sc_dBW-sc_total_tranmission_line_losses-loss_from_duplexer_insertion-double_antenna_transmission_losses;
 
-%uplink
-gs_Antenna_Pointing_loss=0.9;
-Gnd_to_sc_Antenna_Polarization_Loss=3;
+%downlink
+sc_Antenna_Pointing_loss=0;
+sc_to_gs_Antenna_Polarization_Loss=3;
 FSL=20*log10(4*pi*d_0*frequency/c);
 Atmospheric_loss=3;
 Ionospheric_loss=0;
 Rain_loss=0;
-Isotropic_sc=gs_EIRP-Rain_loss-Ionospheric_loss-Atmospheric_loss-FSL-gs_Antenna_Pointing_loss-Gnd_to_sc_Antenna_Polarization_Loss;
+Isotropic_gs=sc_EIRP-Rain_loss-Ionospheric_loss-Atmospheric_loss-FSL-sc_Antenna_Pointing_loss-sc_to_gs_Antenna_Polarization_Loss;
 
-%spacecraft eb/no method
-sc_antenna_pointingloss=0;
-sc_effective_noise_temperature=290; %in K
-sc_figureofmerrit=sc_antenna_gain-sc_total_tranmission_line_losses-10*log10(sc_effective_noise_temperature);
-sc_SNR=sc_figureofmerrit-boltzmann-sc_antenna_pointingloss+Isotropic_sc;
-System_Desired_Data_Rate=64; %kbps
-data_rate_dBHz=10*log10(System_Desired_Data_Rate*1000);
-Telecommand_System_ebno=sc_SNR-data_rate_dBHz;
+%gs eb/no method
+gs_antenna_pointingloss=0.9;
+gs_effective_noise_temperature=290; %in K
+gs_figureofmerrit=sc_antenna_gain-sc_total_tranmission_line_losses-10*log10(gs_effective_noise_temperature);
+gs_SNR=gs_figureofmerrit-boltzmann-gs_antenna_pointingloss+Isotropic_gs;
+System_Desired_Data_Rate=2; %Mbps
+data_rate_dBHz=10*log10(System_Desired_Data_Rate*1000000);
+Telecommand_System_ebno=gs_SNR-data_rate_dBHz;
 BER=1e-5;
 EbNo_theoric=9.6;
-Technological_losses=1;
+Technological_losses=1.5;
 EbNo_Threshold=EbNo_theoric+Technological_losses;
 
 linkmargin=Telecommand_System_ebno-EbNo_Threshold;
--- linkbudgetuplink.m
+++ linkbudgetuplink.m
@@ -1,21 +1,20 @@
-function [linkmargin] = linkbudgetuplink(sc_antenna_gain)
+function [linkmargin] = linkbudgetuplink(u)
 
 %   Detailed explanation goes here
+sc_antenna_gain=u(1);
+range=u(2);
 
 %% Parameters
-global Gtest Azimuth Coelevation
+global Gtest Coelevation gainHF 
 R_earth = 6.3712e+6;
 a = 6678;
 alt_orbit=a*1000-R_earth;
-slant_range=sqrt(R_earth^2+(R_earth+alt_orbit)^2 - (2*R_earth*(R_earth+alt_orbit)*cos(acos(R_earth/(R_earth+alt_orbit))*cos(Coelevation)-Coelevation)));
+d_0=(sqrt((R_earth+alt_orbit)^2-R_earth^2))/1000;
 frequency=2100e6; %Hz
 c=3e8; %m/s celerity
 boltzmann=-226.8;
 
-sc_antenna_gain=Gtest;
-% sc_figureofmerrit=u(2);
-% sc_SNR=u(3);
-% Telecommand_System_ebno=u(4);
+
 
 %Ground Station
 gs_transmitter_power=11.8; % in Watts
@@ -32,19 +31,19 @@ sc_total_tranmission_line_losses=2; %in dB
 %sc_EIRP=sc_antenna_gain+sc_dBW-sc_total_tranmission_line_losses;
 
 %uplink
-Ground_Station_Antenna_Pointing_loss=0.9;
+gs_Antenna_Pointing_loss=0.9;
 Gnd_to_sc_Antenna_Polarization_Loss=3;
-FSL=20*log10(4*pi*slant_range*frequency/c);
+FSL=20*log10(4*pi*range*frequency/c);
 Atmospheric_loss=3;
 Ionospheric_loss=0;
 Rain_loss=0;
-Isotropic_sc=gs_EIRP-Rain_loss-Ionospheric_loss-Atmospheric_loss-FSL-Ground_Station_Antenna_Pointing_loss-Gnd_to_sc_Antenna_Polarization_Loss;
+Isotropic_sc=gs_EIRP-Rain_loss-Ionospheric_loss-Atmospheric_loss-FSL-gs_Antenna_Pointing_loss-Gnd_to_sc_Antenna_Polarization_Loss;
 
 %spacecraft eb/no method
 sc_antenna_pointingloss=0;
 sc_effective_noise_temperature=290; %in K
 sc_figureofmerrit=sc_antenna_gain-sc_total_tranmission_line_losses-10*log10(sc_effective_noise_temperature);
-sc_SNR=-boltzmann+sc_figureofmerrit-sc_antenna_pointingloss+Isotropic_sc;
+sc_SNR=sc_figureofmerrit-boltzmann-sc_antenna_pointingloss+Isotropic_sc;
 System_Desired_Data_Rate=64; %kbps
 data_rate_dBHz=10*log10(System_Desired_Data_Rate*1000);
 Telecommand_System_ebno=sc_SNR-data_rate_dBHz;
